50aa6aa712af9b12a514cce55bae3ae36322ea89
danielbociat <41957946+danielbociat@users.noreply.github.com>
2023-11-20 22:09:38 +0200

    Added Order and Products classes

---
+++ MQT/MQT/Events/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace MQT.Events;
+
+public class Order
+{
+    public DateTime Time { get; set; }
+    public string Id { get; set; }
+    public string ClientFirstName { get; set; }
+    public string ClientLastName { get; set; }
+    public string ClientAddress { get; set; }
+    public IDictionary<Product, int> ProductQuantities { get; set; }
+}
---
+++ MQT/MQT/Events/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace MQT.Events;
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Id{ get; set; }
+}
